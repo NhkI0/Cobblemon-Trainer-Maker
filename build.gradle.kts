500eefd0e20655f17f38796da48c78eacf8da5ac
Dopa <guengant.noe@gmail.com>
2026-02-20 18:56:58 +0100

    feat: app V1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,6 +54,10 @@ jlink {
     imageZip.set(layout.buildDirectory.file("distributions/app-${javafx.platform.classifier}.zip"))
     options.set(listOf("--strip-debug", "--compress", "2", "--no-header-files", "--no-man-pages"))
     launcher {
-        name = "app"
+        name = "CobblemonTrainerMaker"
+    }
+    jpackage {
+        imageName = "CobblemonTrainerMaker"
+        skipInstaller = true
     }
 }
